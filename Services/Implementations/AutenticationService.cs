e56d144769a5e052fddb69632875d6a49ee451c3
SantiagoCaso <santiagoignaciocaso@gmail.com>
2023-11-14 21:53:31 -0300

    Authentication Service

---
+++ Services/Implementations/AutenticationService.cs
@@ -0,0 +1,24 @@
+﻿using Api.Entities;
+
+namespace Api.Services.Implementations
+{
+    public class AutenticationService : ICustomAuthenticationService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AutenticacionService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User? ValidateUser(AuthenticationRequestBody authenticationRequest)
+        {
+            if (string.IsNullOrEmpty(authenticationRequest.UserName) || string.IsNullOrEmpty(authenticationRequest.Password))
+                return null;
+
+            return _userRepository.ValidateUser(authenticationRequest);
+        }
+    }
+}
+
+
--- obj/Release/net6.0/Api.AssemblyInfo.cs
+++ obj/Release/net6.0/Api.AssemblyInfo.cs
@@ -11,7 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("Api")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Api")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
