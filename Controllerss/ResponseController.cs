ee90aa8f0676ce0d2c0f9d9e5ef5501fa0e42a99
LaraSofia <lcristofanelli05@gmail.com>
2023-11-16 22:31:31 -0300

    v4

---
+++ Controllerss/ResponseController.cs
@@ -0,0 +1,50 @@
+﻿using Api.Models.Responses;
+using Api.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Api.Controllerss
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/Consult/{consultationId}/response")]
+    public class ResponseController : ControllerBase
+    {
+        private readonly IResponseService _responseService;
+        private readonly IConsultationService _consultService;
+
+        public ResponseController(IResponseService responseService, IConsultationService consultService)
+        {
+            _responseService = responseService;
+            this._consultService = consultService;
+        }
+
+        [HttpGet("{responseId}", Name = "GetResponse")]
+        public ActionResult<ResponseDTO> GetQuestion(int responseId)
+        {
+            var response = _responseService.GetResponse(responseId);
+
+            if (response is null)
+                return NotFound();
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IActionResult CreateResponse(int consultId, ResponseForCreationDto newResponseForCreation)
+        {
+            if (!_consultService.IsConsultationIdValid(consultId))
+                return NotFound($"Question Id not found: {consultId.ToString()}");
+            var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            var userId = int.Parse(userIdClaim);
+
+            var newResponse = _responseService.CreateResponse(newResponseForCreation, consultId, userId);
+
+            return CreatedAtRoute(
+                "GetResponse",
+                new { consultId = consultId, responseId = newResponse.Id },
+                newResponse);
+        }
+    }
+}
